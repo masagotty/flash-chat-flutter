8079f57531a8d36a55142997526649dc48a51c33
KentaMasago <masagotty@gmail.com>
2020-07-07 07:48:24 +0900

    implemented hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -12,10 +15,16 @@ class FlashChat extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
+          bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: <String, WidgetBuilder>{
+        ChatScreen.id: (BuildContext context) => ChatScreen(),
+        LoginScreen.id: (BuildContext context) => LoginScreen(),
+        RegistrationScreen.id: (BuildContext context) => RegistrationScreen(),
+        WelcomeScreen.id: (BuildContext context) => WelcomeScreen(),
+      },
     );
   }
 }
