1259bf82a81bb50dc916143c92548fc62508d259
KentaMasago <masagotty@gmail.com>
2020-07-09 08:25:26 +0900

    implement tween animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,27 +11,39 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen>
     with SingleTickerProviderStateMixin {
   AnimationController controller;
+  Animation<Color> animation;
+
   @override
   void initState() {
     super.initState();
     controller = AnimationController(
       vsync: this,
       duration: Duration(seconds: 1),
-      upperBound: 100.0,
     );
+
+    animation = ColorTween(
+      begin: Colors.red,
+      end: Colors.blue,
+    ).animate(controller);
 
     controller.forward();
 
     controller.addListener(() {
       setState(() {});
-      print(controller.value);
+      print(animation.value);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -44,7 +56,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: controller.value,
+                    height: 60.0,
                   ),
                 ),
                 Text(
